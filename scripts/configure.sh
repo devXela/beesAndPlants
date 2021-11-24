d46442755f0ed76425b0ddb5b3c87a95e68a9147
Alex Barbosa <alex.barbosa@acpgroup.com.br>
2021-11-24 01:56:23 -0300

    Dockerizando o lumen

---
+++ scripts/configure.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt update
+apt install -y git
+apt install -y zip
+apt install -y sqlite3
+apt install -y bash
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+mv composer.phar /usr/local/bin/composer
+composer create-project -n --prefer-dist laravel/lumen /src/app
+chown 700 $HOME *
+php -S 0.0.0.0:8001 -t /src/app/public
